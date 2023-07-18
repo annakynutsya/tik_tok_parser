f9dff482e60c2da8d9d4e96e70ad037ba84cc338
annakynutsya <kunits.anya@gmail.com>
2023-07-19 00:42:12 +0300

    render fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  ENV['MOZ_HEADLESS_BINARY'] = '/usr/bin/firefox'
+  ENV['MOZ_HEADLESS_BINARY'] = '/usr/lib/firefox/firefox'
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
